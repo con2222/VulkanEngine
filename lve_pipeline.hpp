4422146e6097de9b753cfc4da00793dea48853ee
con2222 <con200956@gmail.com>
2026-03-09 15:24:35 +0300

    Add device setup

--- lve_pipeline.hpp
+++ lve_pipeline.hpp
@@ -1,16 +1,37 @@
 #pragma once
 
+#include "lve_device.hpp"
+
+
+//std
 #include <string>
 #include <vector>
 
 namespace lve {
+
+    struct PipelineConfigInfo {};
+
     class LvePipeline {
-        public:
-            LvePipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+    public:
+        LvePipeline(LveDevice& device, const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& configInfo);
 
-        private:
-            static std::vector<char> readFile(const std::string& filepath);
+        ~LvePipeline() {};
 
-            void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+        LvePipeline(const LvePipeline&) = delete;
+        void operator=(const LvePipeline&) = delete;
+
+        static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
+
+    private:
+        static std::vector<char> readFile(const std::string& filepath);
+
+        void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& config);
+
+        void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+
+        LveDevice& lveDevice;
+        VkPipeline graphicsPipeline;
+        VkShaderModule vertShaderModule;
+        VkShaderModule fragShaderModule;
     };
 }
