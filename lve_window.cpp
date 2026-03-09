28b9b97da3de4f7d881f0213ccabc1d4f2f05cf0
con2222 <con200956@gmail.com>
2026-03-09 10:59:45 +0300

    Init project

---
+++ lve_window.cpp
@@ -0,0 +1,13 @@
+#include "lve_window.hpp"
+
+namespace lve {
+    LveWindow::LveWindow(int w, int h, std::string name) : width(w), height(h), windowName(name) {
+        initWindow();
+    }
+
+    void LveWindow::initWindow() {
+        glfwInit(); // initialize glfw lib
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // not to create opengl context
+
+    }
+}
